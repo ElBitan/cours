aa12c92c224830044285a11ccbd659f4ede4e5c4
ElBitan <93447663+ElBitan@users.noreply.github.com>
2022-07-17 20:54:09 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,5 @@
 const staticCacheName = "cache-v1";
-const assets = ["/", "index.html"];
+const assets = ["/", "index.html",];
 
 // ajout fichiers en cache
 self.addEventListener("install", (e) => {
@@ -53,4 +53,3 @@ self.addEventListener("activate", (e) => {
     })
   );
 });
-
