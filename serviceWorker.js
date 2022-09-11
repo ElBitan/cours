042f66df041268dc4278cedef317fa8e54b38764
ElBitan <93447663+ElBitan@users.noreply.github.com>
2022-09-11 14:27:49 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,55 +1,39 @@
-const staticCacheName = "cache-v1";
-const assets = ["/", "index.html",];
+var cacheName = 'Torah-cours';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+   ...
+];
 
-// ajout fichiers en cache
-self.addEventListener("install", (e) => {
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      cache.addAll(assets);
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
 
-      // IMPORTANT: Cloner la requête.
-      // Une requete est un flux et est à consommation unique
-      // Il est donc nécessaire de copier la requete pour pouvoir l'utiliser et la servir
-      var fetchRequest = event.request.clone();
+self.addEventListener('activate', function(event) {
+  var cacheKeeplist = ['cacheName'];
 
-      return fetch(fetchRequest).then(function (response) {
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key);
         }
-
-        // IMPORTANT: Même constat qu'au dessus, mais pour la mettre en cache
-        var responseToCache = response.clone();
-
-        caches.open(staticCacheName).then(function (cache) {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      });
-    })
-  );
-});
-
-// supprimer caches
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.add(
-        keys
-          .filter((key) => key !== staticCacheName)
-          .map((key) => caches.delete(key))
-      );
+      }));
     })
   );
 });
